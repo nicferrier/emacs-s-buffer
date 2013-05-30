fd76cc144a9d7d06b057397a3f618e1bbbef16bd
Nic Ferier <nic@ferrier.me.uk>
2013-05-31 00:06:58 +0100

    add url and bump the version

--- s-buffer.el
+++ s-buffer.el
@@ -5,8 +5,9 @@
 ;; Author: Nic Ferrier <nferrier@ferrier.me.uk>
 ;; Keywords: lisp
 ;; Created: 15th May 2013
+;; Url: http://github.com/nicferrier/emacs-s-buffer
 ;; Package-requires: ((s "1.6.0")(noflet "0.0.1"))
-;; Version: 0.0.1
+;; Version: 0.0.2
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
