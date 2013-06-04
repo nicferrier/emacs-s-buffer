781e53fc3b4082e8d4be717d5986838bf225dbb6
Nic Ferier <nic@ferrier.me.uk>
2013-06-04 22:25:13 +0100

    bump the version, simplify it a lot.

--- s-buffer.el
+++ s-buffer.el
@@ -6,8 +6,8 @@
 ;; Keywords: lisp
 ;; Created: 15th May 2013
 ;; Url: http://github.com/nicferrier/emacs-s-buffer
-;; Package-requires: ((s "1.6.0")(noflet "0.0.1"))
-;; Version: 0.0.2
+;; Package-requires: ((s "1.6.0")(noflet "0.0.3"))
+;; Version: 0.0.3
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -31,49 +31,14 @@
 ;;; Code:
 
 (require 's)
-(require 'noflet)
-
-(defun s-buffer-format (buffer replacer &optional extra)
-  "Format the text in BUFFER in place with REPLACER.
-
-The arguments for format-buffer are as for `s-format'."
-  (noflet ((replace-regexp-in-string
-            (re replcr str &optional fc lit)
-            (with-current-buffer
-                (get-buffer (get-text-property 0 :buffer str))
-              (save-excursion
-                (goto-char (get-text-property 0 :point str))
-                (while (re-search-forward re nil t)
-                  (noflet ((replace-regexp-in-string
-                            (re fn str &optional fc lit)
-                            (funcall this-fn re fn str fc lit)))
-                          (replace-match (funcall replcr nil) nil t)))))))
-          (s-format
-           (propertize
-            "dummystr"
-            :buffer buffer
-            :point (with-current-buffer buffer (point)))
-           replacer extra)
-          buffer))
-
-(defun s-buffer|format-internal (original-s-format str replacer extra)
-  (noflet ((s-format
-            (str replacer &optional extra)
-            (funcall original-s-format str replacer extra)))
-      (s-buffer-format str replacer extra)))
 
 (defmacro s-buffer-lex-format (buffer)
   "Use scope to resolve the variables in BUFFER.
 
 The buffer form of `s-lex-format'."
-  (let ((bufsym (make-symbol "bv")))
-    `(let ((,bufsym ,buffer))
-       (noflet ((s-format
-                 (str replacer &optional extra)
-                 (s-buffer|format-internal
-                  this-fn str replacer extra)))
-               (s-lex-format ,bufsym)
-               ,bufsym))))
+  `(s-lex-format (with-current-buffer (eval ,buffer)
+                   (buffer-string))))
+
 
 (provide 's-buffer)
 
